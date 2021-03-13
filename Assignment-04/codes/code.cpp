75b43a9b1b5aeffd13aebdfb8359758518e49a9c
suryaskant <suryaskant29@gmail.com>
2021-03-13 15:11:07 +0530

    taking input from user

---
+++ Assignment-04/codes/code.cpp
@@ -0,0 +1,33 @@
+/*
+    Problem Statement
+    Given a set of strings, find the longest common prefix.(Using divide and conquer algorithm)
+        -> Use random functions to generate elements.
+    
+    // Right now we are taking all the elements (string, num) from user
+    // but in later phase, we will replace it with random functions to generate random input
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray(string arr[],int n){
+    // print all the elements of given array
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<"\n";
+    }
+}
+
+int main(){
+    // how many strings are given in input ?
+    int numberOfStrings;
+    cin>>numberOfStrings;
+
+    // an array of strings to store all the strings
+    string arr[numberOfStrings];
+    
+    for(int i=0;i<numberOfStrings;++i){
+        // taking strings from user as input and storing it into the string array
+        cin>>arr[i];
+    }
+    // Testing if all the inputs have been successfully stored in array
+    printArray(arr,numberOfStrings);
+}

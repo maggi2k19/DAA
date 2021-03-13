2493d5c9194b3ed967d725aac8794f9c874f50cc
suryaskant <suryaskant29@gmail.com>
2021-03-13 16:05:22 +0530

    added comments to commonPrefix function

--- Assignment-04/codes/code.cpp
+++ Assignment-04/codes/code.cpp
@@ -24,12 +24,29 @@ string commonPrefix(string str1, string str2){
     string ans = "";
 
     // printing both the strings to check if the string is passed successfully 
-<<<<<<< HEAD
     // cout<<str1<<"--\n"<<str2<<"--\n";
-=======
-    cout<<str1<<"--\n"<<str2<<"--\n";
->>>>>>> 5f701ffb7e85234d828b7d6c6747cc49da6c3452
 
+    // n1 : size of 1st string
+    // n2 : size of second string
+    int n1 = str1.size(), n2 = str2.size();
+
+    // i : current index of first string
+    // j : current index of second string
+    int i=0,j=0;
+    while(i<n1 && j<n2){
+        if(str1[i]==str2[j]){
+            // if character at both strings is equal then it will be included in our common prefix
+            ans.push_back(str1[i]);
+            i++,j++;
+        }
+        else{
+            // if current index of both strings is not equal 
+            // we cannot get any longer commong prefix
+            break;
+        }
+    }
+    // ans stores our common prefix
+    // return it
     return ans;
 }
 
@@ -37,14 +54,6 @@ string solveLCP(string arr[],int start,int end){
     // this function is the solution of our problem statement
     // it will return the LCP (longest common prefix) of given array of string
     // we will use divide and conquer algorithm
-    
-    // start is the starting index of array
-    // end is the last valid index of our array
-
-    // printing the array to check if the string is passed successfully
-    for(int i=start;i<=end;++i){
-        cout<<arr[i]<<" ";
-    }
 
     return "";
 }
@@ -67,13 +76,5 @@ int main(){
     // printArray(arr,numberOfStrings);
 
     // sample call to commonPrefix function before working on it 
-<<<<<<< HEAD
     // cout<<commonPrefix("abcdef","abcd");
-
-    // sample call to solveLCP function before working on it 
-    solveLCP(arr,0,numberOfStrings-1);
 }
-=======
-    cout<<commonPrefix("abcdef","abcd");
-}
->>>>>>> 5f701ffb7e85234d828b7d6c6747cc49da6c3452

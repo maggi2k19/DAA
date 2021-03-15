06d403cdb626936a02763ecc67a030fa5f77723b
suryaskant <suryaskant29@gmail.com>
2021-03-15 13:54:17 +0530

    cleaned the code, removed some comment lines

--- Assignment-04/codes/code.cpp
+++ Assignment-04/codes/code.cpp
@@ -2,10 +2,16 @@
     Problem Statement
     Given a set of strings, find the longest common prefix.(Using divide and conquer algorithm)
         -> Use random functions to generate elements.
-    
-    // Right now we are taking all the elements (string, num) from user
-    // but in later phase, we will replace it with random functions to generate random input
+
+    // we have used three functions to generate random elements 
+        // getRandomNumber(l,r) : return a random number number between l to r 
+        // getRandomChar() : return a random character between a-z or A-Z
+        // getRandomString() : return a random string of variable length, using above two functions
+
+    // we can also take input from user
+        // for that we just have to uncomment the input codes, and comment the codes for random test generation
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -35,9 +41,7 @@ char getRandomChar(){
 
 string getRandomString(int n){
     // get a random string of length n
-
     string randomString = "";
-
     for(int i=0;i<n;++i){
         char c = getRandomChar();
         randomString.push_back(c);
@@ -46,14 +50,11 @@ string getRandomString(int n){
 }
 
 string commonPrefix(string str1, string str2){
-    // the motive of this function is to return the 
+    // this function returns
     // commonPrefix (longest) of any two given string
 
     // ans will store the commonPrefix between str1 and str2
     string ans = "";
-
-    // printing both the strings to check if the string is passed successfully 
-    // cout<<str1<<"--\n"<<str2<<"--\n";
 
     // n1 : size of 1st string
     // n2 : size of second string
@@ -82,14 +83,8 @@ string commonPrefix(string str1, string str2){
 }
 
 string solveLCP(string arr[],int start,int end){
-    // this function is the solution of our problem statement
-    // it will return the LCP (longest common prefix) of given array of string
+    // solveLCP function will return the LCP (longest common prefix) of given array of string
     // we will use divide and conquer algorithm
-
-    // printing the array to check if the string is passed successfully
-    // for(int i=start;i<=end;++i){
-    //     cout<<arr[i]<<" ";
-    // }
 
     string ans = "";
     if(start==end){
@@ -104,7 +99,7 @@ string solveLCP(string arr[],int start,int end){
         // str1 is the LCP of all the strings from start-mid index
         string str1 = solveLCP(arr,start,mid);
 
-        // str1 is the LCP of remaining strings (mid to end)
+        // str2 is the LCP of remaining strings (mid to end)
         string str2 = solveLCP(arr,mid+1,end);
 
         // commongPrefix of these two strings from left side and right side of array
@@ -118,15 +113,9 @@ string solveLCP(string arr[],int start,int end){
 }
 
 
-int main(){
-    srand(time(NULL)); 
-    // how many strings are given in input ?
-    // cout<<getRandomNumber(5,10);
-    // return 0;
-    // cout<<getRandomChar()<<"\n";
-    // return 0;
-    // cout<<getRandomString(5)<<"\n";
-    // return 0;
+int main(){ 
+    // to generate a different random number every time
+    srand(time(NULL));
 
     // cout<<"Enter count of strings : \n";
     int numberOfStrings;
@@ -146,18 +135,10 @@ int main(){
     cout<<"--------------------------------------------\n";
     printArray(arr,numberOfStrings);
     cout<<"--------------------------------------------";
-    // Testing if all the inputs have been successfully stored in array
-    // test successfull ! 
-    // printArray(arr,numberOfStrings);
 
-    // sample call to commonPrefix function before working on it 
-    // commonPrefix is complete
-    // cout<<commonPrefix("abcdef","abcd");
-
-    // sample call to solveLCP function before working on it
-    // cout<<solveLCP(arr,0,numberOfStrings-1);
-
+    // LCP stores the longest common prefix of all strings
     string LCP = solveLCP(arr,0,numberOfStrings-1);
+
     if(LCP.size()==0){
         cout<<"\nNo common prefix \n";
     }

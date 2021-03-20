20f8ff2a4c03686ae5da83200a8fb06d51ccdc21
Meghana Santhoshi Kandagatla <58394323+maggi2k19@users.noreply.github.com>
2021-03-20 19:06:31 +0530

    Update code.cpp

--- Assignment-04/codes/code.cpp
+++ Assignment-04/codes/code.cpp
@@ -119,7 +119,7 @@ int main(){
 
     // cout<<"Enter count of strings : \n";
     int numberOfStrings;
-    numberOfStrings = getRandomNumber(1,1000);
+    numberOfStrings = getRandomNumber(1,10);
     // cin>>numberOfStrings;
 
     // an array of strings to store all the strings
